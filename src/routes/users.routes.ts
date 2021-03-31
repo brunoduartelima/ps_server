3ca981d40b2b554fa2b3b26b4eb64f6be43532b1
Bruno Duarte <bruno-sacra@hotmail.com>
2021-03-31 18:22:05 -0300

    creating UsersController

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,28 +1,7 @@
 import { Router } from 'express';
 
-import CreateUserService from '@modules/users/services/CreateUserService';
-
 const usersRouter = Router();
 
-usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, cpf, phone, email, password } = request.body;
-
-        const createUser = new CreateUserService();
-
-        const user = await createUser.execute({
-            name,
-            cpf,
-            phone,
-            email,
-            password,
-        });
-
-        return response.json(user);
-
-    } catch (error) {
-        
-    }
-})
+usersRouter.post('/');
 
 export default usersRouter;
