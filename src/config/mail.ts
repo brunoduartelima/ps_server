633f854df9d5c3f8af2105c46397b486724df744
Bruno Duarte <bruno-sacra@hotmail.com>
2021-05-25 19:20:25 -0300

    MailTemplateProvider and mail config

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+    driver: 'ethereal' | 'ses';
+
+    defaults: {
+        from: {
+            email: string;
+            name: string;
+        };
+    };
+}
+
+export default {
+    driver: process.env.MAIL_DRIVER || 'ethereal',
+
+    defaults: {
+        from: {
+            email: 'bruno@psmanager.com.br',
+            name:  'Bruno do Personal Manager',
+        },
+    },
+} as IMailConfig;
