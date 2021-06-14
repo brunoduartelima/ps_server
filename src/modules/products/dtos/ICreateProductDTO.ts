3b9a75c9e886ee23ca225b7759b937ee36009024
Bruno Duarte <bruno-sacra@hotmail.com>
2021-06-14 18:59:54 -0300

    create productDTO

---
+++ src/modules/products/dtos/ICreateProductDTO.ts
@@ -0,0 +1,9 @@
+export default interface ICreateProductDTO {
+    name: string;
+    code: string;
+    description: string;
+    price: number;
+    quantity: number;
+    average_cost: number;
+    shop_id: string;
+}
